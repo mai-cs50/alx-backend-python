c8da83a5a416f714e1261812ff9360d485e1c151
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-10 21:56:18 +0300

    d

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function make_multiplier"""
+
+
+from typing import List, Iterable, Sequence, TUple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """takes a list of sequence and int"""
+    return [(i, len(i)) for i in 1st]
