8ef0b08b82bce3e8b86802dbe330102f04f0f329
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-10 20:00:14 +0300

    d

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
+"""Write a type-annotated function"""
+
+
 def concat(str1: str, str2: str) -> str:
+    """takes a string as arguments and returns a concatenated string"""
     return str1 + str2
