5b4832382101de46927ef450fc2e54e210118de6
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-10 23:50:27 +0300

    d

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,7 +2,7 @@
 """Write a type-annotated function to_kv"""
 
 
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
