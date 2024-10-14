93ee067ab0b363327b0d901518be7449d4bb16c7
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-14 20:00:15 +0300

    s

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Write an asynchronous coroutine"""
+"""Import wait_random from the previous python file that you’ve written"""
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """"""
+    """spawn wait_random n times with the specified max_delay."""
     futures = [wait_random(max_delay) for _ in range(n)]
     futures = asyncio.as_completed(futures)
     delays = [await future for future in futures]
