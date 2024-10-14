76333b36a9c2e90e692550cb7f8f419170c3c281
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-14 21:15:10 +0300

    s

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 """Import wait_random from the previous python file that you’ve written"""
 import asyncio
-from typing import List
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> List[float]:
+def measure_time(n: int, max_delay: int) -> float:
     """spawn wait_random n times with the specified max_delay."""
-    start_time = time3perf_counter()
-    futures = asyncio.as_completed(futures)
-    delays = [await future for future in futures]
-    return delays
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.perf_counter()
+    return (end_time - start_time) / n
